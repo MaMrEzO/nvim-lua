4a868a7b5dd81c6cc0d2257281a9e889373960f2
Nima Zare <nima85shz@gmail.com>
2023-03-07 15:32:45 +0330

    add mapping for surrounding visual text

--- lua/user/mapping/mapping.lua
+++ lua/user/mapping/mapping.lua
@@ -3,6 +3,9 @@ local opts = require'user.configs.mapping'
 vim.keymap.set('v', "<Tab>", '>gv', opts)
 vim.keymap.set('v', "<S-Tab>", '<gv', opts)
 
---vnoremap <TAB> >gv
---vnoremap <S-TAB> <gv
-
+vim.keymap.set('v', "(", '"xc(<C-r><C-o>x)<esc>', opts)
+vim.keymap.set('v', "'", '"xc\'<C-r><C-o>x\'<esc>', opts)
+vim.keymap.set('v', '"', '"xc"<C-r><C-o>x"<esc>', opts)
+vim.keymap.set('v', '`', '"xc`<C-r><C-o>x`<esc>', opts)
+vim.keymap.set('v', "[", '"xc[<C-r><C-o>x]<esc>', opts)
+vim.keymap.set('v', "{", '"xc{<CR><C-r><C-o>x<CR>}<esc>', opts)
